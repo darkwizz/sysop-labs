6782d0317c083907d6a678dcfaaab807ab207926
darkwizz <psyhoge@gmail.com>
2021-01-18 22:57:57 +0100

    print <You> for messages written by the logged in user

--- lab-5/chat.c
+++ lab-5/chat.c
@@ -31,28 +31,40 @@ int main(int argc, char** argv)
         printf("Too long username\n");
         return -2;
     }
+
+    if (strstr(argv[1], "#") != NULL || strstr(argv[1], "<") != NULL ||
+    strstr(argv[1], ">") != NULL)
+    {
+        printf("#, <, > cannot be in a username\n");
+        return -7;
+    }
     
     strcpy(USERNAME, argv[1]);
     printf("Hello, %s\n", USERNAME);
 
-    chat_sem = sem_open("/CHAT_SEM_NEW", O_CREAT, S_IRUSR |
+    chat_sem = sem_open("/CHAT_SEMAPHORE", O_CREAT, S_IRUSR |
         S_IWUSR | S_IRGRP | S_IWGRP, 1);
     if (chat_sem == SEM_FAILED)
     {
         printf("Cannot open semaphore, error %s\n", strerror(errno));
         return -5;
     }
-    printf("Open semaphore successfully\n");
+    // printf("Open semaphore successfully\n");
     read_sent_messages();
 
     char message[240];
     while (1)
     {
-        printf("You: ");
+        printf("You (or type EXIT): ");
         scanf("%[^\n]%*c", message);
+        if (strcmp("EXIT", message) == 0)
+        {
+            break;
+        }
         send_message(USERNAME, message);
     }
 
+    sem_close(chat_sem);
     return 0;
 }
 
@@ -70,9 +82,23 @@ void read_sent_messages()
     char* line = NULL;
     size_t len = 0;
     ssize_t read;
+    char sep[] = "#";
     while ((read = getline(&line, &len, messages)) != -1)
     {
+        char* tempstr = calloc(strlen(line) + 1, sizeof(char));
+        strcpy(tempstr, line);
+        char* msg_username = strtok(tempstr, sep);
+        char* msg_rest = strtok(NULL, sep);
+        if (strcmp(msg_username, USERNAME) == 0)
+        {
+            char* changed_line = calloc(strlen(msg_rest) + strlen("<You>#") + 1, sizeof(char));
+            sprintf(changed_line, "%s#%s", "<You>", msg_rest);
+            printf("%s", changed_line);
+            free(changed_line);
+            continue;
+        }
         printf("%s", line);
+        free(tempstr);
     }
     fclose(messages);
     sem_post(chat_sem);
